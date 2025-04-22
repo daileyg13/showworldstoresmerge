6adc40e22467f5967b58d6940973af0c5eb1a15d
daileyg13 <daileyg13@gmail.com>
2025-04-22 14:41:48 -0400

    greece zip

--- app/greece/page.tsx
+++ app/greece/page.tsx
@@ -5,7 +5,7 @@ export default function GreeceStore() {
     <StorePageLayout
       title="Show World - Greece"
       description="Visit our Greece location for the full Show World experience!"
-      address={`1765 Mt Reed Blvd\nRochester, NY 14615`}
+      address={`1765 Mt Reed Blvd\nRochester, NY 14606`}
       mapLink="https://www.google.com/maps/dir/?api=1&destination=1765+Mt+Reed+Blvd+Rochester+NY+14615"
       phone="(585) 254-0600"
     />
