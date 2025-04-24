abea7ed5135dce3a6ccc3058ae40fde026c144df
daileyg13 <daileyg13@gmail.com>
2025-04-24 14:28:34 -0400

    Update Footer.tsx
    added link to home page in footer logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,16 @@
-export default function Footer() {
-    return (
-      <footer className="bg-zinc-950 py-6 text-center text-sm text-zinc-400 mt-20">
-        <div className="flex flex-col items-center gap-2">
+export default function Footer() { 
+  return (
+    <footer className="bg-zinc-950 py-6 text-center text-sm text-zinc-400 mt-20">
+      <div className="flex flex-col items-center gap-2">
+        <a href="/">
           <img
             src="/Images/ShowWorld girl pink.png"
             alt="Show World Logo"
             className="h-6"
           />
-          <p>&copy; 2025 Show World. All rights reserved.</p>
-        </div>
-      </footer>
-    );
-  }
-  
+        </a>
+        <p>&copy; 2025 Show World. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
