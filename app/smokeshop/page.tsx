063fd60144163e65412e455ca56cbb2846376b91
daileyg13 <daileyg13@gmail.com>
2025-04-27 01:20:48 -0400

    Update page.tsx

--- app/smokeshop/page.tsx
+++ app/smokeshop/page.tsx
@@ -9,7 +9,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Show World Smoke Shop | Pipes, Vapes & Smoking Accessories",
-  description: "Explore premium pipes, vapes, rolling papers, and smoking accessories at Show World’s Smoke Shop. Top brands, 24/7 shopping, all in one place!"
+  description: "Explore premium pipes, vapes, rolling papers, and smoking accessories at Show World’s Smoke Shop. Top brands, 24/7 head shop shopping, all in one place!"
 }
 
 const brandLogos = [
