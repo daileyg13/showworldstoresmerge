f6b64cccdcb23c4c4c0ae63ae79d8949e758a8c8
daileyg13 <daileyg13@gmail.com>
2025-12-04 13:44:57 -0500

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+// app/not-found.tsx
+export default function NotFound() {
+  return (
+    <main style={{ padding: "4rem", textAlign: "center" }}>
+      <h1>Page not found</h1>
+      <p>Sorry, we couldn&apos;t find that page.</p>
+    </main>
+  );
+}
